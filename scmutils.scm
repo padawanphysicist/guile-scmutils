0d6346e2bd7849163ea4fd2067737c0e997945b9
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2024-01-02 21:48:22 -0300

    apparently its working!

--- scmutils.scm
+++ scmutils.scm
@@ -25,6 +25,20 @@
 
 (define-module (scmutils))
 
+(eval-when (load compile eval)
+           (load-from-path "./scmutils/load.scm"))
+
+
+;; Add all bindings from generic-environment to the current module
+(module-for-each (lambda (sym var)
+                   (module-add! (current-module) sym var))
+                 generic-environment)
+
+;; Export bindings -- hack to just export everything
+(module-for-each (lambda (sym var)
+                   (module-replace! (current-module) (list sym)))
+                 (current-module))
+
 (define-syntax re-export-modules
   (syntax-rules ()
     ((_ (mod ...) ...)
